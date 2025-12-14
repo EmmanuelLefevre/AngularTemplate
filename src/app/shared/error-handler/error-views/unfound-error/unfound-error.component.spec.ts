82de174340c8781c280f9f46d1d96dcdc14523ea
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-14 10:41:22 +0100

    add unfound-error component

---
+++ src/app/shared/error-handler/error-views/unfound-error/unfound-error.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UnfoundErrorComponent } from './unfound-error.component';
+
+describe('UnfoundErrorComponent', () => {
+  let component: UnfoundErrorComponent;
+  let fixture: ComponentFixture<UnfoundErrorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UnfoundErrorComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UnfoundErrorComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/error-handler/error-views/unfound-error/unfound-error.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'unfound-error',
+  imports: [],
+  templateUrl: './unfound-error.component.html',
+  styleUrl: './unfound-error.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UnfoundErrorComponent {}
