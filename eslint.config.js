7c2377e446979026aadc9bb41a6529a74350bc04
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-20 17:33:39 +0100

    chore: fix eslint stylistic config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,25 @@
-// @ts-check
 const eslint = require('@eslint/js');
 const { defineConfig } = require('eslint/config');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const stylistic = require('@stylistic/eslint-plugin');
 
 module.exports = defineConfig([
+  // TS ----------
   {
     files: ['**/*.ts'],
+    plugins: {
+      '@stylistic': stylistic
+    },
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended
     ],
     processor: angular.processInlineTemplates,
     rules: {
+      // Angular selectors configuration
       '@angular-eslint/component-selector': [
         'error',
         {
@@ -31,8 +36,10 @@ module.exports = defineConfig([
           style: 'camelCase'
         }
       ],
-      '@typescript-eslint/brace-style': ['error', 'stroustrup'],
-      '@typescript-eslint/padding-line-between-statements': [
+      // Stylistics selectors configuration
+      '@stylistic/brace-style': ['error', 'stroustrup'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/padding-line-between-statements': [
         'error',
         {
           blankLine: 'always',
@@ -40,12 +47,20 @@ module.exports = defineConfig([
           next: 'class'
         }
       ],
-      indent: ['error', 2]
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single'],
+      // Typescript selectors configuration
+      // Commons
+      // Disabling old rules to avoid duplicates
+      indent: 'off',
+      semi: 'off',
+      quotes: 'off'
     }
   },
+  // HTML ----------
   {
     files: ['**/*.html'],
-    extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {
       '@angular-eslint/template/component-selector': 'off',
       '@angular-eslint/template/directive-selector': 'off'
