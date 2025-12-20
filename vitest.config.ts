957b9c1168288f2e04b050e9df069408f4231ff0
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-20 23:20:52 +0100

    test again

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,13 +6,14 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       enabled: true,
-      reporter: ['text', 'lcov', 'html'],
+      reporter: ['text', 'lcov', 'clover', 'html'],
       reportsDirectory: './coverage',
       exclude: [
         'src/test-setup.ts',
         'src/main.ts',
         '**/*.spec.ts',
-        '**/*.module.ts'
+        '**/*.module.ts',
+        '.angular/**'
       ]
     }
   }
