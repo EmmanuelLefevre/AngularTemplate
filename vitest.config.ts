cdbcde95a45061813eb0fe0e0c5faaba5d279b57
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-20 19:58:18 +0100

    maj

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'v8',
+      provider: 'istanbul',
+      skipFull: true,
       reporter: ['text', 'lcov', 'html'],
       reportsDirectory: './coverage',
       exclude: [
