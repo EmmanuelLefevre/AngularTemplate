1eabf0b3acc83826cb622d78f156e81ba4d85d5a
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-21 19:41:25 +0100

    add component

---
+++ src/app/shared/components/footer/main-footer.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'main-footer',
+  imports: [],
+  templateUrl: './main-footer.component.html',
+  styleUrl: './main-footer.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class MainFooterComponent {}
