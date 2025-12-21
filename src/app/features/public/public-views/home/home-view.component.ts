dcf779be9bec0b0537df851a2310da5d513c814f
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-21 19:41:09 +0100

    add component

---
+++ src/app/features/public/public-views/home/home-view.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'home-view',
+  imports: [CommonModule],
+  templateUrl: './home-view.component.html',
+  styleUrl: './home-view.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class HomeViewComponent {
+
+  readonly title = signal('AngularTemplate');
+
+  readonly resources = [
+    { title: 'Explore the Docs', link: 'https://angular.dev' },
+    { title: 'Learn with Tutorials', link: 'https://angular.dev/tutorials' },
+    { title: 'CLI Docs', link: 'https://angular.dev/tools/cli' },
+    // ...
+  ];
+}
