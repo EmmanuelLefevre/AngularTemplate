1960da51de9cb1d193b10523ac69acccefb8db8f
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-23 21:31:11 +0100

    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,23 +3,23 @@ import angular from '@analogjs/vite-plugin-angular';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
-import path from 'path';
+// import path from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  resolve: {
-    alias: [
-      {
-        find: '@core',
-        replacement: path.resolve(__dirname, 'src/app/core')  
-      },
-      {
-        find: '@app',
-        replacement: path.resolve(__dirname, 'src/app')
-      }
-    ],
-  },
+  // resolve: {
+  //   alias: [
+  //     {
+  //       find: '@core',
+  //       replacement: path.resolve(__dirname, 'src/app/core')
+  //     },
+  //     {
+  //       find: '@app',
+  //       replacement: path.resolve(__dirname, 'src/app')
+  //     }
+  //   ],
+  // },
   plugins: [
     angular(),
     tsconfigPaths({
@@ -36,7 +36,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: [resolve(__dirname, 'src/test-setup.ts')],
+    setupFiles: [
+      resolve(__dirname, 'src/app/core/_config/navlinks.constant.ts'),
+      resolve(__dirname, 'src/test-setup.ts'),
+    ],
     reporters: ['default'],
     coverage: {
       provider: 'v8',
