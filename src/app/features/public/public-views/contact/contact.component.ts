74f2d967dcb7b9648bff65cf0693c041bfecd3ca
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-27 18:47:20 +0100

    add contact component

---
+++ src/app/features/public/public-views/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'contact',
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContactComponent {
+
+}
