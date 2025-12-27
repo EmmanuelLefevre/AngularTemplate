dccdbd3600a767a121677f71185371cff5c1a0fd
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-27 18:24:14 +0100

    add new footer

--- src/app/shared/components/footer/main-footer.component.ts
+++ src/app/shared/components/footer/main-footer.component.ts
@@ -1,11 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateModule } from '@ngx-translate/core';
+import { FOOTER_SOCIAL_LINKS, EXTERNAL_LINKS } from '@app/core/_config/social-links.constant';
 
 @Component({
   selector: 'main-footer',
-  imports: [],
+  imports: [
+    FontAwesomeModule,
+    RouterModule,
+    TranslateModule
+  ],
   templateUrl: './main-footer.component.html',
   styleUrl: './main-footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class MainFooterComponent {}
+export class MainFooterComponent {
+  protected readonly socialLinks = FOOTER_SOCIAL_LINKS;
+  protected readonly externalLinks = EXTERNAL_LINKS;
+  protected readonly currentYear = new Date().getFullYear();
+}
