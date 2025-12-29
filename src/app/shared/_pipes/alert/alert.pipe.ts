8cd77a170c8a07dcf1611653aeff08404ce7faca
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-30 00:16:29 +0100

    fix sonar quality gate

--- src/app/shared/_pipes/alert/alert.pipe.ts
+++ src/app/shared/_pipes/alert/alert.pipe.ts
@@ -6,7 +6,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class AlertPipe implements PipeTransform {
 
-  transform(value: boolean): string {
-    return value ? 'Alerte !' : '-';
+  private readonly ALERT_TEXT = 'Alerte !';
+  private readonly DEFAULT_TEXT = '-';
+
+  transform(hasAlert: boolean): string {
+    return hasAlert ? this.getAlertLabel() : this.getDefaultLabel();
+  }
+
+  private getAlertLabel(): string {
+    return this.ALERT_TEXT;
+  }
+
+  private getDefaultLabel(): string {
+    return this.DEFAULT_TEXT;
   }
 }
