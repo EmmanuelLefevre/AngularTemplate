1e7fd96e5b6045e902ffda75bffb22480cd5ea1b
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-29 23:18:59 +0100

    add mock admin login button on home

--- src/app/features/public/public-layout.component.ts
+++ src/app/features/public/public-layout.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { MockAdminLoginButtonComponent } from '@app/shared/components/dev/mock-admin-login-button/mock-admin-login-button.component';
 import { SHARED_LAYOUT_COMPONENTS } from '@shared';
 
 @Component({
   selector: 'public-layout',
   imports: [
+    MockAdminLoginButtonComponent,
     RouterOutlet,
     TranslateModule,
     ...SHARED_LAYOUT_COMPONENTS
