324e02d1ca8e6ac85acf656a2676f79a6fbb9eed
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-29 22:02:19 +0100

    inithAuth at application start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+
+import { AuthService } from '@core/_services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +11,20 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrl: './app.component.scss'
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   /* v8 ignore start */
   protected readonly title = signal('AngularTemplate');
   /* v8 ignore stop */
   private readonly translate = inject(TranslateService);
 
+  private readonly authService = inject(AuthService);
+
   constructor() {
     const BROWSER_LANG = this.translate.getBrowserLang();
     this.translate.use(BROWSER_LANG?.match(/en|fr/) ? BROWSER_LANG : 'fr');
+  }
+
+  ngOnInit(): void {
+    this.authService.initAuth();
   }
 }
