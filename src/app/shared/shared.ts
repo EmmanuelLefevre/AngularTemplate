32b6a8a3add2f9e9ead4a5d35e9e7165027f6180
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-29 17:25:14 +0100

    new approach to export singles

--- src/app/shared/shared.ts
+++ src/app/shared/shared.ts
@@ -1,30 +1,3 @@
-// ========== EXPORTS ==========
-// --- Errors Management
-export * from './error-handler/error-handler.component';
-export * from './error-handler/error-views/generic-error/generic-error.component';
-export * from './error-handler/error-views/server-error/server-error.component';
-export * from './error-handler/error-views/unauthorized-error/unauthorized-error.component';
-export * from './error-handler/error-views/unfound-error/unfound-error.component';
-export * from './error-handler/error-views/unknown-error/unknown-error.component';
-
-// --- Layout Components
-export * from './components/footer/main-footer.component';
-export * from './components/header/header-nav.component';
-
-// --- UI
-export * from './components/language-toggle/language-toggle.component';
-
-// --- Directives
-export * from './_directives/input-focus/input-focus.directive';
-export * from './_directives/input-trim/input-trim.directive';
-export * from './_directives/input-uppercase/input-uppercase.directive';
-
-// --- Pipes
-export * from './_pipes/alert/alert.pipe';
-export * from './_pipes/date/date-format.pipe';
-export * from './_pipes/yes-no/yes-no.pipe';
-
-// ========== IMPORTS ==========
 // --- Errors Management
 import { ErrorHandlerComponent } from './error-handler/error-handler.component';
 import { GenericErrorComponent } from './error-handler/error-views/generic-error/generic-error.component';
@@ -49,6 +22,8 @@ import { InputUppercaseDirective } from './_directives/input-uppercase/input-upp
 import { AlertPipe } from './_pipes/alert/alert.pipe';
 import { DateFormatPipe } from './_pipes/date/date-format.pipe';
 import { YesNoPipe } from './_pipes/yes-no/yes-no.pipe';
+
+// --- Utils
 
 export const SHARED_ERRORS_COMPONENTS = [
   ErrorHandlerComponent,
@@ -81,5 +56,29 @@ export const SHARED_PIPES = [
 ] as const;
 
 export const SHARED_UTILS = [
-  // MyUtils
+  // My utils...
 ] as const;
+
+export {
+  // --- Errors Management
+  ErrorHandlerComponent,
+  GenericErrorComponent,
+  ServerErrorComponent,
+  UnauthorizedErrorComponent,
+  UnfoundErrorComponent,
+  UnknownErrorComponent,
+  // --- Layout Components
+  HeaderNavComponent,
+  MainFooterComponent,
+  // --- UI Components
+  LanguageToggleComponent,
+  // --- Directives
+  InputFocusDirective,
+  InputTrimDirective,
+  InputUppercaseDirective,
+  // --- Pipes
+  AlertPipe,
+  DateFormatPipe,
+  YesNoPipe,
+  // --- Utils
+};
