3ef1467348fe8fdc20bab14ea0d50ec543fe353b
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-29 18:51:11 +0100

    works + tests on admin layout

--- src/app/features/admin/admin-layout.component.ts
+++ src/app/features/admin/admin-layout.component.ts
@@ -1,12 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { AuthService } from '@core/_services/auth/auth.service';
 
 @Component({
   selector: 'admin',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class AdminLayoutComponent {}
+export class AdminLayoutComponent {
+  public readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
+}
