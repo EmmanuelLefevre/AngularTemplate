7b53cb93f4b12b800f2cfd6c33bd4be6ab57a29e
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-29 21:10:34 +0100

    mock boolean

--- src/_environments/environment.prod.sample.ts
+++ src/_environments/environment.prod.sample.ts
@@ -1,5 +1,6 @@
 export const ENVIRONMENT = {
   production: true,
   apiUrl: 'https://api.your-domain.com/api',
+  useMocks: false,
   logLevel: 'error'
 };
--- src/_environments/environment.ts
+++ src/_environments/environment.ts
@@ -1,5 +1,6 @@
 export const ENVIRONMENT = {
   production: false,
+  useMocks: true,
   apiUrl: 'http://localhost:3000/api',
   logLevel: 'debug'
 };
