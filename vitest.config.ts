48b92db32e9a2eef1469e7738c9d6f0668371c3d
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2025-12-31 21:48:54 +0100

    add threshold 80% test mini on pipeline

--- vitest.config.ts
+++ vitest.config.ts
@@ -32,6 +32,12 @@ export default defineConfig({
       enabled: true,
       reporter: ['text', 'lcov', 'html'],
       reportsDirectory: './coverage',
+      thresholds: {
+        statements: 80,
+        branches: 80,
+        functions: 80,
+        lines: 80,
+      },
       exclude: [
         '**/index.ts',
         '.angular/**',
