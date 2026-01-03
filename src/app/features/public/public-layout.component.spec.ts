dcc6a3b4816d55ce54741d43669eeb58ce54e753
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2026-01-03 23:58:17 +0100

    100% coverage

--- src/app/features/public/public-layout.component.spec.ts
+++ src/app/features/public/public-layout.component.spec.ts
@@ -2,12 +2,19 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { AuthService } from '@core/_services/auth/auth.service';
+
 import { PublicLayoutComponent } from './public-layout.component';
 
 describe('PublicLayoutComponent', () => {
 
   let component: PublicLayoutComponent;
   let fixture: ComponentFixture<PublicLayoutComponent>;
+
+  const AUTH_SERVICE_MOCK = {
+    isAuthenticated: vi.fn(() => false),
+    currentUser: vi.fn(() => null)
+  };
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
@@ -16,13 +23,16 @@ describe('PublicLayoutComponent', () => {
         TranslateModule.forRoot()
       ],
       providers: [
-        provideRouter([])
+        provideRouter([]),
+        { provide: AuthService, useValue: AUTH_SERVICE_MOCK }
       ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PublicLayoutComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
+    await fixture.whenStable();
   });
 
   it('should create', () => {
