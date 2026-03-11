1ca253bf39cccb064a084f14c1255e279ed80ed3
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2026-03-11 18:05:08 +0100

    maj

--- src/app/shared/error-handler/error-views/unauthorized-error/unauthorized-error.component.ts
+++ src/app/shared/error-handler/error-views/unauthorized-error/unauthorized-error.component.ts
@@ -1,10 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, OnInit, DestroyRef } from '@angular/core';
+
+import { TypeWriterDirective } from '@shared/_directives/typewriter/typewriter.directive';
 
 @Component({
   selector: 'unauthorized-error',
-  imports: [],
+  imports: [
+    TypeWriterDirective,
+  ],
   templateUrl: './unauthorized-error.component.html',
   styleUrl: './unauthorized-error.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class UnauthorizedErrorComponent {}
+
+export class UnauthorizedErrorComponent implements OnInit {
+
+  private readonly el = inject(ElementRef);
+  private readonly destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const mouseMoveListener = (event: MouseEvent): void => {
+      const container = this.el.nativeElement.querySelector('.interrogation-room');
+      if (!container) return;
+
+      const rect = container.getBoundingClientRect();
+
+      const x = event.clientX - rect.left;
+      const y = event.clientY - rect.top;
+
+      container.style.setProperty('--mouse-x', `${x}px`);
+      container.style.setProperty('--mouse-y', `${y}px`);
+    };
+
+    document.addEventListener('mousemove', mouseMoveListener);
+
+    this.destroyRef.onDestroy(() => {
+      document.removeEventListener('mousemove', mouseMoveListener);
+    });
+  }
+}
