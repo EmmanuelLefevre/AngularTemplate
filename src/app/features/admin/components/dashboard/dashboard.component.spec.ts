4d8fc6bbb6dc8dd136b3eb56190311328a430117
Emmanuel Lefevre <47084975+EmmanuelLefevre@users.noreply.github.com>
2026-04-01 18:51:17 +0200

    fix pipeline

--- src/app/features/admin/components/dashboard/dashboard.component.spec.ts
+++ src/app/features/admin/components/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { signal } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { AuthService } from '@core/_services/auth/auth.service';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -7,13 +12,29 @@ describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
+  const AUTH_SERVICE_MOCK = {
+    currentUser: signal({ username: 'Admin', roles: ['ADMIN'] }),
+    isAdmin: signal(true),
+    isAuthenticated: signal(true)
+  };
+
   beforeEach(async() => {
+
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [
+        DashboardComponent,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        provideRouter([]),
+        { provide: AuthService, useValue: AUTH_SERVICE_MOCK }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
